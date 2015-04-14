fb2bd07c226adbfd4dd60f02bd776050cc6664fd
Chris <chris.carty@gmail.com>
2015-04-13 22:02:38 -0400

    finished adding bing_search.py. Chapter 15

--- rango/bing_search.py
+++ rango/bing_search.py
@@ -19,4 +19,43 @@ def run_query(search_terms):
 	query = urllib.quote(query)
 
 	#Format backhalf of Url and set jSON formatting
-	search_url = "{0}{1}?$format=json$top={2}&$skip={3}&"
+	search_url = "{0}{1}?$format=json$top={2}&$skip={3}&$Query={4}".format(
+			root_url,
+			source,
+			results_per_page,
+			offset,
+			query
+		)
+
+	#User name must be blank. Don't forget to enter your API Key
+	username = ''
+
+	password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+	password_mgr.add_password(None, search_url, username, BING_API_KEY)
+
+	result = []
+
+	try:
+		#Prepare API Communication
+		handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+		opener = urllib2.builder_opener(handler)
+
+		urllib2.install_opener(opener)
+
+		#connect to the server
+		response = urllib2.urlopen(search_url).read()
+
+		json_response = json.loads(response)
+
+		#Loop through each page returned and populate results
+		for result in json_response['d']['results']:
+			results.append({
+				'title': result['title'],
+				'link' : result['Url'],
+				'summary' : result['Description']
+				})
+	except urllib2.URlError, e:
+		print "Error connecting to the bing API"
+
+	#return the results
+	return results
