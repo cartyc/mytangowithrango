808e09afaedf8d26ca6d72e2d62fb66df87f1b8e
Chris <chris.carty@gmail.com>
2015-04-18 22:33:33 -0400

    Finished chapter 17

--- static/js/rango-jquery.js
+++ static/js/rango-jquery.js
@@ -1,3 +1,18 @@
-$(document).ready(fuction(){
-	
+// $(document).ready( function(){
+
+// 	$("#about-btn").click(function(event){
+// 		alert("You clicked the button using JQUERY!");
+// 	});
+// });
+
+$(document).ready( function() {
+
+    $("#about-btn").addClass('btn btn-primary');
+
+    $("#about-btn").click( function(event){
+	msgstr = $("#msg").html()
+	msgstr = msgstr + "0"
+	$("#msg").html(msgstr)
 });
+});
+
